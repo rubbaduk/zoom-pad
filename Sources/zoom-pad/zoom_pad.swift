fec3c1279049f6551a9fe7c2d2bb2350a1cedeab
DAVIN DO <davin4do@gmail.com>
2025-10-26 19:55:06 +0800

    initialised swift project

---
+++ Sources/zoom-pad/zoom_pad.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+
+@main
+struct zoom_pad {
+    static func main() {
+        print("Hello, world!")
+    }
+}
