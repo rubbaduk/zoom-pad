018f7895d0868bb0fdc7c2359668040f55c2471c
DAVIN DO <davin4do@gmail.com>
2025-10-26 20:37:47 +0800

    added prompt to request accessibility permissions

--- Sources/zoom-pad/zoom_pad.swift
+++ Sources/zoom-pad/zoom_pad.swift
@@ -1,10 +1,26 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import ApplicationServices
 import Foundation
+
+private func requestAccessibility() {
+    // prompt for accessibility permission
+    let opts =
+        [
+            "AXTrustedCheckOptionPrompt" as CFString: true
+        ] as CFDictionary
+    let trusted = AXIsProcessTrustedWithOptions(opts)
+
+    if trusted {
+        return print("Access granted")
+    } else {
+        return print("Follow prompt to allow access")
+    }
+    // ref: https://developer.apple.com/documentation/applicationservices/1459186-axisprocesstrustedwithoptions
+
+}
 
 @main
 struct zoom_pad {
     static func main() {
-        print("Hello, world!")
+        requestAccessibility()
     }
 }
